dd137765e8b7a02e5f603150582ad2c188bbea49
roinaveiro <roi.naveiro@gmail.com>
2020-10-19 18:43:42 +0200

    code simulator

---
+++ simulator.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+
+
+class simulator:
+    """
+    Class to simulate road
+    Args:
+        l (int): road lenght
+    """
+
+    def __init__(self, l):
+        self._l = l
+        ##
+        self._road_dynamics = pd.read_csv("data/road_state_evol", index_col=0, delim_whitespace=True)
+        ##
+        self._driver_dynamics = pd.read_csv("data/driver_state_evol", delim_whitespace=True)
+        self._driver_dynamics.set_index(["Current", "Obstacle"], inplace=True)
+        ##
+        self._driver_char = pd.read_csv("data/driver_char", index_col=0, delim_whitespace=True)
+
+    def simulate_road(self):
+
+        road = np.empty(self._l, dtype=object)
+        road[0] = "Clean"
+
+        for i in range(1, self._l):
+            p = self._road_dynamics.loc[ road[i-1] ]
+            road[i] = np.random.choice(self._road_dynamics.columns, p = p)
+
+        return road
+
+    def simulate_driver_state(self, road):
+
+        driver = np.empty(self._l, dtype=object)
+        driver[0] = "Aware"
+
+        for i in range(1, self._l):
+            p = self._driver_dynamics.loc[ (driver[i-1], road[i]) ]
+            driver[i] = np.random.choice(self._driver_dynamics.columns, p = p)
+
+        return driver
+    
+    def simulate_driver_char(self, driver):
+
+        driver_char = np.empty(self._l, dtype=int)
+        
+        for i in range(self._l):
+            p = self._driver_char.loc[ driver[i] ]
+            driver_char[i] = np.random.choice(self._driver_char.columns.astype(int), p = p)
+        
+        return driver_char
+
+    def simulate_environment(self):
+        
+        road          = self.simulate_road()
+        driver        = self.simulate_driver_state(road)
+        driver_char   = self.simulate_driver_char(driver)
+
+        return {"road" : road, "driver" : driver, "driver_char" : driver_char}
---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from simulator import simulator
+
+
+sim = simulator(200)
+print("hola")
