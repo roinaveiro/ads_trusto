983a6d755bdd1ac798e0f7cdfd5373c53203d783
roinaveiro <roi.naveiro@gmail.com>
2020-11-15 17:37:17 +0100

    add final simulations

--- experiments1.py
+++ experiments1.py
@@ -3,7 +3,7 @@ import pandas as pd
 from simulator import simulator
 from ads import ADS
 
-N_sim = 100
+N_sim = 10
 results = []
 
 for i in range(N_sim):
@@ -24,4 +24,4 @@ for i in range(N_sim):
     results.append(ads.get_info())
 
 df = pd.DataFrame(results)
-df.to_csv("results/sim2.csv", index=False)
+df.to_csv("results/sim4_less.csv", index=False)
